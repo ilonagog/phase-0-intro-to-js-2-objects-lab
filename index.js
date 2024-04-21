478d736e56cf7d7c3344d9a042015eee4825c4d8
ilona <gogiashvili.ilona@gmail.com>
2024-04-21 17:42:10 -0400

    commit tree 198034db865e8895f8942e47ab3595585d886276

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+  name: "Ilona",
+  streetAddress: "2370 Ocean Av",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+const newEmployee = updateEmployeeWithKeyAndValue(
+  employee,
+  "Ilia",
+  "345 Elmcroft BLVD"
+);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+const updateEmployee = destructivelyUpdateEmployeeWithKeyAndValue(
+  employee,
+  "name",
+  "Sofi"
+);
+updateEmployee();
+function deleteFromEmployeeByKey(employee, key) {
+  const deleteEmployeeKey = { ...employee };
+  delete deleteEmployeeKey[key];
+  return deleteEmployeeKey;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
